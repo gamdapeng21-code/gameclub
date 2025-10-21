d57495d05c4497da9973ade30fed2ebfc40c8361
gamdapeng21-code <gamdapeng21@gmail.com>
2025-10-22 02:05:35 +0800

    添加: 创建缺失的 Next.js 页面文件解决构建错误

---
+++ pages/_error.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Error({ statusCode }) {
+  return (
+    <p>
+      {statusCode
+        ? `服务器端发生错误 ${statusCode}`
+        : '客户端发生错误'}
+    </p>
+  );
+}
+
+Error.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
+
+export default Error;
